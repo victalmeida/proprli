74d7d3072c280195fcfa44e8e79c166de9963dc9
victor <vh.almeida93@gmail.com>
2024-08-21 13:21:23 -0300

    Upload Tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,24 @@
 
 namespace Tests;
 
+use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+
+use Laravel\Sanctum\Sanctum;
+use App\Models\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->withoutExceptionHandling();
+    }
+
+    public function auth(int $userID){
+        $user = User::find($userID);
+        Sanctum::actingAs($user, [], 'api');
+    }
 }
